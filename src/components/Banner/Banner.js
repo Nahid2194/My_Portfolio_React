eee9276284b536e893cfed911dccaac5bedd1e4d
Nahid2194 <nahidjc80@gmail.com>
2021-03-15 20:49:17 +0600

    Banner complete

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Banner.css'
+const Banner = () => {
+    return (
+        <div>
+            <section className="banner-container container-fluid">
+                <div className="row vh-100">
+                    <div className="col-md-9 banner-column m-auto text-center text-white p-5  border border-light">
+                        <h1 className="display-4 font-weight-bolder">I'AM NAHID HASAN</h1>
+                        <h3>Student | Programmer | Cricketer</h3>
+
+                    </div>
+
+                </div>
+
+            </section>
+        </div>
+    );
+};
+
+export default Banner;
